3e8522328ff2b1ef59dad4fc4daded17b6bcc163
Kenneth Ang <ksami.ihide@gmail.com>
2013-12-08 20:40:26 +0800

    initial commit working read/write

---
+++ src/com/ksami/simpletxt/EditTextActivity.java
@@ -0,0 +1,121 @@
+package com.ksami.simpletxt;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.ksami.simpletxt.MainActivity;
+import com.ksami.simpletxt.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.support.v4.app.NavUtils;
+
+public class EditTextActivity extends Activity {
+	
+	private static String filePath;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_text);
+		// Show the Up button in the action bar.
+		setupActionBar();
+		
+		// Get fileName passed from previous activity
+		Intent intent=getIntent();
+		String fileName=intent.getStringExtra(MainActivity.FILENAME);
+		filePath=fileName+".txt";
+		
+		readFromFile();
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}.
+	 */
+	private void setupActionBar() {
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.edit_text, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			
+			writeToFile();
+			
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	// Read from file and display content
+	private void readFromFile() {
+		try {
+			BufferedReader readStream = new BufferedReader(new FileReader(getFilesDir()+File.separator+filePath));
+			
+			EditText textContent=(EditText) findViewById(R.id.edit_text);
+			String lineToDisplay;
+			while((lineToDisplay = readStream.readLine()) != null) {
+				textContent.append(lineToDisplay);
+			}
+			
+			readStream.close();
+		}
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	// Save changes on exit or up level or every x secs
+	private void writeToFile() {
+		try {
+			BufferedWriter writeStream = new BufferedWriter(new FileWriter(getFilesDir()+File.separator+filePath));
+			
+			EditText textContent=(EditText) findViewById(R.id.edit_text);
+			String textToWrite = textContent.getText().toString();
+			for(int i=0; i<textToWrite.length(); i++)
+			{
+				writeStream.write(textToWrite.charAt(i));
+			}
+			
+			writeStream.close();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/com/ksami/simpletxt/MainActivity.java
@@ -0,0 +1,76 @@
+package com.ksami.simpletxt;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	
+	public static final String FILENAME = "com.ksami.simpletxt.FILENAME";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		case R.id.action_new_file:
+			// TODO stub
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void list_item1_click(View view) {
+		// Start next activity for editing indicated text file
+		Intent intent = new Intent(this, EditTextActivity.class);
+		Button listItem = (Button) findViewById(R.id.list_item1);
+		String fileName = listItem.getText().toString();
+		
+    	//Attach data to pass to next activity to intent object
+    	intent.putExtra(FILENAME, fileName);
+    	
+    	//Start activity specified by intent
+    	startActivity(intent);
+	}
+	
+	public void list_item2_click(View view) {
+		// Start next activity for editing indicated text file
+		Intent intent = new Intent(this, EditTextActivity.class);
+		Button listItem = (Button) findViewById(R.id.list_item2);
+		String fileName = listItem.getText().toString();
+		
+    	//Attach data to pass to next activity to intent object
+    	intent.putExtra(FILENAME, fileName);
+    	
+    	//Start activity specified by intent
+    	startActivity(intent);
+	}
+
+}
