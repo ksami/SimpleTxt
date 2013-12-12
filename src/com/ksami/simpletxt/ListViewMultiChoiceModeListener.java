333c831f447a2e4f8e87728b04f1dd574545540f
Kenneth Ang <ksami.ihide@gmail.com>
2013-12-12 18:03:31 +0800

    implement delete single file

--- src/com/ksami/simpletxt/ListViewMultiChoiceModeListener.java
+++ src/com/ksami/simpletxt/ListViewMultiChoiceModeListener.java
@@ -1,13 +1,19 @@
 package com.ksami.simpletxt;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.AbsListView.MultiChoiceModeListener;
+import android.widget.Toast;
 
+// Implements contextual action bar for rename and delete
 public class ListViewMultiChoiceModeListener implements MultiChoiceModeListener {
 	private SelectionAdapter mSelection;
 	
@@ -34,7 +40,7 @@ public class ListViewMultiChoiceModeListener implements MultiChoiceModeListener 
         // Respond to clicks on the actions in the CAB
         switch (item.getItemId()) {
             case R.id.action_delete:
-            	// TODO deleteSelected();
+            	deleteSelected(mSelection);
                 mSelection.clearSelection();
                 mode.finish(); // Action picked, so close the CAB
                 return true;
@@ -45,6 +51,32 @@ public class ListViewMultiChoiceModeListener implements MultiChoiceModeListener 
             default:
                 return false;
         }
+    }
+    
+    private void deleteSelected(SelectionAdapter mSelection) {
+    	Set<Integer> checkedItems = mSelection.getCurrentCheckedPosition();
+    	
+    	assert(checkedItems.isEmpty() == false);
+    	
+    	Iterator<Integer> iter =checkedItems.iterator();
+    	while(iter.hasNext()) {
+    		int itemPosition = iter.next();
+    		try {
+	    		File fileToDelete = new File(mSelection.getContext().getFilesDir()+File.separator+mSelection.getItem(itemPosition)+".txt");
+	    		if(fileToDelete.delete()) {
+	    			Toast toast = Toast.makeText(mSelection.getContext(), "File deleted", Toast.LENGTH_SHORT);
+	    			toast.show();
+	    		}
+	    		else {
+	    			throw new IOException();
+	    		}
+	    		mSelection.removeSelection(itemPosition);
+	    		mSelection.notifyDataSetChanged();
+    		}
+    		catch(IOException e) {
+    			e.printStackTrace();
+    		}
+    	}
     }
 
     @Override
@@ -59,6 +91,7 @@ public class ListViewMultiChoiceModeListener implements MultiChoiceModeListener 
     public void onDestroyActionMode(ActionMode mode) {
         // Here you can make any necessary updates to the activity when
         // the CAB is removed. By default, selected items are deselected/unchecked.
+    	mSelection.clearSelection();
     }
 
     @Override
